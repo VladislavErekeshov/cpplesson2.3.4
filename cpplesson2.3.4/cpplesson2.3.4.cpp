ecca93dfc7eb340fe964cb8318118590abda2ada
VladislavErekeshov <tommysyching@gmail.com>
2021-02-18 06:39:57 +0300

    Добавьте файлы проекта.

---
+++ cpplesson2.3.4/cpplesson2.3.4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x, a, b, c, d, e;
+    cin >> x;
+    a = x / 10000 % 10;
+    b = x / 1000 % 10;
+    c = x / 100 % 10;
+    d = x / 10 % 10;
+    e = x / 1 % 10;
+    cout << e * 10000 + d * 1000 + c * 100 + b * 10 + a;
+    return 0;
+}
